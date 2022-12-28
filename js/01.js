3fc85d8f1877b6ce8b7cc50fac014f4421b5e6dd
LuisMariscalDev <luisscortees@gmail.com>
2022-12-28 16:03:37 -0600

    variables

---
+++ js/01.js
@@ -0,0 +1,27 @@
+// Variables
+
+var producto = "Audifonos gamer"; //Creando la variable y asignandole valor
+
+var disponible; //Creando la variable pero sin valor
+
+// producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+
+var producto1 = "Computadora",
+    disponible = true,
+    categoria = "Computadoras";
+
+
+// var 1disponible;
+var disponible_;
+
+//Estilos para las variables
+var nombre_producto = 'Monitor HD'; //underscore
+var nombreProducto = 'Tele 4K'; // Camel Case
+var NombreProducto = 'Xbox'; // Pascal Case
+var nombreproducto = 'play 5 chipeada'; // no tiene nombre 
+
+
+console.log(producto);
