3c0531b5a62cc28341dd6550e8e730873ae56022
LuisMariscalDev <luisferdev16@gmail.com>
2022-12-28 17:57:26 -0600

    String and string methods

---
+++ js/04.js
@@ -0,0 +1,18 @@
+// String o Cadenas de Texto
+
+const tweet = "Aprendiendo JavaScript con el curso de Desarrollo de Web Completo";
+const producto2 = "Monitor HD";
+const email = "correo@c";
+
+//length es para la externsion (verifica cuantos caracteres tiene la cadena y nos lo muestra)
+console.log(tweet.length);
+console.log(producto2.length);
+
+//IndexOf (Nos dice en que posicion inicia la palabra que estamos buscando)
+console.log(tweet.indexOf("curso"));
+console.log(producto2.indexOf("Tablet"));
+console.log(email.indexOf("@"));
+
+//Includes retorna true o false dependiendo si está o no la palabra buscada
+console.log(tweet.indexOf("curso"));
+console.log(producto2.indexOf("Tablet"));
