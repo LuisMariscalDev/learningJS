0dcd839d306b4a9c4902586f38f5a38d47b94ed1
LuisMariscalDev <luisferdev16@gmail.com>
2022-12-28 16:28:23 -0600

    let and const

---
+++ js/02.js
@@ -0,0 +1,24 @@
+// Variables con let
+
+let producto = "Audifonos gamer"; //Creando la variable y asignandole valor
+
+let disponible; //Creando la variable pero sin valor
+
+// producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+
+let producto1 = "Computadora",
+    disponible1 = true,
+    categoria = "Computadoras";
+
+
+// let 1disponible;
+let disponible_;
+
+//Estilos para las variables
+let nombre_producto = 'Monitor HD'; //underscore
+let nombreProducto = 'Tele 4K'; // Camel Case
+let NombreProducto = 'Xbox'; // Pascal Case
+let nombreproducto = 'play 5 chipeada'; // no tiene nombre 
---
+++ js/03.js
@@ -0,0 +1,24 @@
+// Variables con const
+
+const producto = "Audifonos gamer"; //Creando la variable y asignandole valor
+
+let disponible = 0; //Creando la variable pero sin valor
+
+// producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+
+const producto1 = "Computadora",
+    disponible1 = true,
+    categoria = "Computadoras";
+
+
+// const 1disponible;
+const disponible_ = true;
+
+//Estilos para las variables
+const nombre_producto = 'Monitor HD'; //underscore
+const nombreProducto = 'Tele 4K'; // Camel Case
+const NombreProducto = 'Xbox'; // Pascal Case
+const nombreproducto = 'play 5 chipeada'; // no tiene nombre 
