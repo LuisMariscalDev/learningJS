99bb3e98c802a5f0e4402e77269e765e70da7546
LuisMariscalDev <luisferdev16@gmail.com>
2022-12-28 18:41:25 -0600

    numbers and method .Math

---
+++ js/06.js
@@ -0,0 +1,16 @@
+//Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.6);
+resultado = Math.ceil(2.1);
+resultado = Math.floor(2.9);
+resultado = Math.sqrt(144);
+resultado = Math.abs(-4);
+resultado = Math.min(3, 2, 5, 6, 4, 9, 44)
+resultado = Math.max(3, 2, 5, 6, 4, 9, 44)
+resultado = Math.random();
+resultado = Math.floor( Math.random() * 50 );
+
+console.log(resultado);
