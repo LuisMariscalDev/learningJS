485fd87b29e462b07e65b0395e02aa4bef19ac96
LuisMariscalDev <luisferdev16@gmail.com>
2022-12-28 20:22:22 -0600

    contatenacion y string literals

---
+++ js/08.js
@@ -0,0 +1,9 @@
+const nombre = "Luis";
+const email = "correo@correo.com";
+
+// Concatenación
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+
+//Template String o Strings Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
